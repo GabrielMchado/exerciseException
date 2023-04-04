850532ea44b89e9765addd0565c99f1307bc3cc1
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-04-04 08:14:58 -0300

    One version

---
+++ src/model/exception/WhitdrawException.java
@@ -0,0 +1,11 @@
+package model.exception;
+
+public class WhitdrawException extends Exception {
+	
+		private static final long serialVersionUID = 1L;
+		
+		public WhitdrawException(String msg) {
+			super(msg);
+		}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module accountExercicio {
+}
